cf85f703e178c1d880b88026838ed3cfe87822f2
alx <acriptis@gmail.com>
2020-11-26 16:48:15 +0300

    improve view for export, import and retrain

--- ic_dataset/views.py
+++ ic_dataset/views.py
@@ -1,25 +1,38 @@
-from django.shortcuts import render
+# import sys
 import os
+import json
+import tempfile
 from django.conf import settings
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from ic_dataset.from_icjson_2_db import update_from_ic_ds_formatted_dict
 from .forms import UploadFileForm
+from ic_dataset.from_db_2_icjson import export_db_2_ic_json
+from django.http import FileResponse
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# sys.path.append(ROOT_DIR)
 
 
 def download_intent_phrases(request):
     """View for exporting from DB to intent_phrases.json"""
-    from .from_db_2_icjson import ds_path
-
+    fd, ds_path = tempfile.mkstemp(suffix='.json')
+    # try:
+    export_db_2_ic_json(ds_path)
     if os.path.exists(ds_path):
-        with open(ds_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="application/json")
-            response['Content-Disposition'] = 'inline; filename=' + os.path.basename(ds_path)
-            return response
+        response = FileResponse(open(ds_path, 'rb'), as_attachment=True,
+                                filename="intent_phrases_export.json")
+        return response
     raise Http404
+    # finally:
+    #     os.remove(ds_path)
+    #     raise Http404
 
 
 def upload_intents_json_view(request):
+    """
+    Handles uploading of intent_phrase.json file and exporting intents to DB
+    """
 
     message = 'Upload your intent_phrases.json!'
     # Handle file upload
@@ -28,8 +41,7 @@ def upload_intents_json_view(request):
         if form.is_valid():
             # got a file
             print("got a file with content...")
-            # content = request.FILES['file'].read()
-            import json
+
             data = json.load(request.FILES['file'])
             print("JSON:")
             print(data)
@@ -46,6 +58,12 @@ def upload_intents_json_view(request):
     return render(request, 'intents_upload.html', context)
 
 def train_model_view(request):
+    from ic_dataset.tasks import dp_retrain_task
+    from ic_dataset.models import calc_dataset_hash
+    hash = calc_dataset_hash()
     context = {
-        'message': "Model is launched for training. Visit api in 5 minutes..."}
+        'message': f"Model is launched for training. It's hash code is: {hash}. Visit api in about 25 minutes..."
+    }
+
+    dp_retrain_task.delay()
     return render(request, 'train_model.html', context)
