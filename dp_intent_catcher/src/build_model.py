91678d08813f8eaa8aa420aabdca0d2b960eb208
alx <acriptis@gmail.com>
2020-11-26 15:02:30 +0300

    update building model for multifolder mode

--- dp_intent_catcher/src/build_model.py
+++ dp_intent_catcher/src/build_model.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
-
+import os, inspect, sys
+import glob
 from os import getenv
-
 import tensorflow as tf
 import logging
-# import sentry_sdk
-from flask import Flask, request, jsonify
 
 from .detector import *
 
@@ -18,24 +16,47 @@ logger = logging.getLogger(__name__)
 class IntentCatcherClassifier():
     def __init__(self):
         self.sess = tf.compat.v1.Session()
-
         logger.info('Creating detector...')
-        self.detector = RegMD(logger)
-        logger.info('Creating detector... finished')
+        ############################################# Universal Import ##################################
+        current_abs_path = os.path.abspath(inspect.getfile(inspect.currentframe()))
+        DP_INTENT_CATCHER_PATH = os.path.dirname(os.path.dirname(current_abs_path))
+        # print("DP_INTENT_CATCHER_PATH ")
+        # print(DP_INTENT_CATCHER_PATH )
 
-        logger.info('Initializing tf variables...')
-        self.sess.run(tf.compat.v1.tables_initializer())
+        search_dir = DP_INTENT_CATCHER_PATH + "/data/models/*"
+        # print(search_dir)
+        dirs = list(filter(os.path.isdir, glob.glob(search_dir)))
+        paths = sorted(dirs, key=lambda x: os.path.getmtime(x))
+        print("Found models:")
+        print(paths)
 
-        logger.info("Tables initialized")
-        self.sess.run(tf.compat.v1.global_variables_initializer())
-        logger.info("Global variables initialized")
+        # try to open any model that can be opened. By the newest:
+        for model_dir_path in paths:
+            try:
+                self.detector = RegMD(logger, intent_model_dir_path=model_dir_path)
+                logger.info('Creating detector... finished')
 
-        # self.detector.detect([["Wake up phrase"]], self.sess)
-        logger.info("DONE")
+                logger.info('Initializing tf variables...')
+                self.sess.run(tf.compat.v1.tables_initializer())
+
+                logger.info("Tables initialized")
+                self.sess.run(tf.compat.v1.global_variables_initializer())
+                logger.info("Global variables initialized")
+
+                # self.detector.detect([["Wake up phrase"]], self.sess)
+                logger.info(f"Intent Cacther initialization is done for {model_dir_path}!")
+                print(f"Model is launched: {model_dir_path}")
+                break
+            except Exception as e:
+                print(e)
+        else:
+            # print(no models found, launch default:
+            print("can not launch any model!")
+            raise Exception("No model launchable")
+
 
     def predict(self, utterance):
-        # utterances = "My friend want to talk about basketball"
-        # logger.info(f"Number of utterances: {len(utterances)}")
+        # TODO detect if model updated
         results = self.detector.detect(utterance, self.sess)
         print("results:")
         print(results)
