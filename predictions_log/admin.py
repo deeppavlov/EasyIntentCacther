3fa82453233c49bc60d9555fdd85e9d0aeab58ce
alx <acriptis@gmail.com>
2020-12-03 19:31:53 +0300

    make readonly fields for prediction log

--- predictions_log/admin.py
+++ predictions_log/admin.py
@@ -7,6 +7,9 @@ from predictions_log.models import PredictionCase
 class PredictionCaseAdmin(admin.ModelAdmin):
     search_fields = ['input_data']
     list_display = ('input_data', 'prediction_data', 'count', 'last_updated_at')
-    # pass
+    readonly_fields = ('input_data', 'prediction_data', 'count', 'last_updated_at', 'other_kwargs', 'created_at')
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 admin.site.register(PredictionCase, PredictionCaseAdmin)
